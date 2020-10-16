810fbd06e75a1ef8c12d9f3a3630bfd7c895313c
Yufei Pan <pan.712@osu.edu>
2020-10-16 16:59:21 -0400

    Update sshLogger.sh

--- sshLogger.sh
+++ sshLogger.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 grep -a "Failed password" /var/log/auth.log > sshFailedAttempts.txt
-grep -E -o -a "([0-9]{1,3}[\.]){3}[0-9]{1,3}" sshFailedAttempts.txt | sort | uniq -c | sort -g -r > failedIPs.txt
-grep -o -a "Failed password for .* from" sshFailedAttempts.txt | sort | uniq -c | sort -g -r | awk '{print $1, $(NF-1) }' > failedUsers.txt
+echo "counts   ip" > failedIPs.txt
+grep -E -o -a "([0-9]{1,3}[\.]){3}[0-9]{1,3}" sshFailedAttempts.txt | sort | uniq -c | sort -g -r >> failedIPs.txt
+echo "counts   user" > failedUsers.txt
+grep -o -a "Failed password for .* from" sshFailedAttempts.txt | sort | uniq -c | sort -g -r | awk '{print $1, $(NF-1) }' >> failedUsers.txt
