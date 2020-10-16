f1c53dd6d05257643bc0ddebe7626ee7262784c5
Yufei Pan <pan.712@osu.edu>
2020-10-16 16:52:41 -0400

    Update sshLogger.sh
    Update the logger so that it actually pull and format the logs

--- sshLogger.sh
+++ sshLogger.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo "Hello World"
+grep -a "Failed password" /var/log/auth.log > sshFailedAttempts.txt
+grep -E -o -a "([0-9]{1,3}[\.]){3}[0-9]{1,3}" sshFailedAttempts.txt | sort | uniq -c | sort -g -r > failedIPs.txt
+grep -o -a "Failed password for .* from" sshFailedAttempts.txt | sort | uniq -c | sort -g -r | awk '{print $1, $(NF-1) }' > failedUsers.txt
