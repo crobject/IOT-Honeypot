4e72197b76828f58132d0ad5599d839263b65674
Yufei Pan <pan.712@osu.edu>
2020-11-10 03:37:16 -0500

    Create scheduler.sh
    this scheduler would add a crontab entry to the root user to execute chugger.log every 30 minutes

---
+++ scheduler.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Author Yufei Pan
+#
+# Execute chugger.sh every 30 minutes and save the log to chugger.log
+#
+# save the current directory name to dir
+dir=$(pwd)
+# Aquire super user permission if not executed from user root
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+echo "Adding " $dir"/chugger.sh to crontab tasks"
+# Using the root user as not all server is set up to have another user other than root (not recommended but for cross platform compatibility using root here)
+echo "*/30 * * * * $dir/chugger.sh > $dir/chugger.log" | tee -a /var/spool/cron/root
